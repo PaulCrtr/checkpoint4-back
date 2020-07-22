b93f99df8990325834cee479e0b06d714c43eed5
PaulCrtr <paul.th.cartier@gmail.com>
2020-07-23 00:58:31 +0200

    Post full recipe done

--- routes/recipeRoute.js
+++ routes/recipeRoute.js
@@ -37,7 +37,7 @@ recipeRouter.get(
   (req, res, next) => {
     const recipeId = req.params.recipeId;
     db.query(
-      "SELECT I.* FROM Ingredient AS I JOIN Ingredient_has_Recipe AS has ON has.Recipe_id_recipe = ?",
+      "SELECT I.* FROM Ingredient AS I JOIN Ingredient_has_Recipe AS has ON has.Recipe_id_recipe = ? WHERE I.id_ingredient = has.Ingredient_id_ingredient",
       recipeId,
       (error, results) => {
         if (error) {
@@ -79,27 +79,115 @@ recipeRouter.get(
   }
 );
 
-// POST ONE
-recipeRouter.post("/", (req, res) => {
-  const { name_recipe } = req.body;
-  if (name_recipe) {
-    db.query("INSERT INTO Recipe SET ?", req.body, (error, results) => {
-      if (error) {
-        console.log(error);
-        res.status(500).json({ status: "internal server error" });
-      } else {
-        res.status(201).json({
-          status: "success",
-          recipeCreated: { id: results.insertId, ...req.body },
-        });
+// POST ONE FULL
+recipeRouter.post(
+  "/",
+  (req, res, next) => {
+    const {
+      name_recipe,
+      time_recipe,
+      number_recipe,
+      image,
+      author,
+      ingredients,
+      instructions,
+    } = req.body;
+    if (
+      name_recipe &&
+      time_recipe &&
+      number_recipe &&
+      author &&
+      ingredients &&
+      instructions
+    ) {
+      const data = { name_recipe, time_recipe, number_recipe, image, author };
+      db.query("INSERT INTO Recipe SET ?", data, (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ status: "internal server error" });
+        } else {
+          // res.status(201).json({
+          //   status: "success",
+          //   recipeCreated: { id: results.insertId, ...req.body },
+          // });
+          req.body.recipeCreated = { id: results.insertId, ...data };
+          next();
+        }
+      });
+    } else {
+      res.status(422).json({
+        status: "error",
+        status: "required field(s) missing",
+      });
+    }
+  },
+  (req, res, next) => {
+    const data = [...req.body.instructions].map((element) => [
+      ...element,
+      req.body.recipeCreated.id,
+    ]);
+    db.query(
+      "INSERT INTO Instruction (content_instruction, order_instruction, Recipe_id_recipe) VALUES ?",
+      [data],
+      (error, _) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ status: "internal server error" });
+        } else {
+          req.body.recipeCreated = {
+            ...req.body.recipeCreated,
+            instructions: data,
+          };
+          next();
+        }
       }
-    });
-  } else {
-    res.status(422).json({
-      status: "error",
-      status: "required field(s) missing",
-    });
+    );
+  },
+  (req, res, next) => {
+    const data = req.body.ingredients;
+    db.query(
+      "INSERT INTO Ingredient (name_ingredient) VALUES ?",
+      [data],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ status: "internal server error" });
+        } else {
+          const ingredientsHasRecipe = [...data].map((e, i) => {
+            return [
+              req.body.recipeCreated.id,
+              i + 1 + results.insertId - results.affectedRows,
+            ];
+          });
+          req.body.recipeCreated = {
+            ...req.body.recipeCreated,
+            ...data,
+            ingredientsHasRecipe,
+          };
+          next();
+        }
+      }
+    );
+  },
+  (req, res) => {
+    const data = req.body.recipeCreated.ingredientsHasRecipe;
+    console.log(data);
+    db.query(
+      "INSERT INTO Ingredient_has_Recipe (Recipe_id_recipe, Ingredient_id_ingredient) VALUES ?",
+      [data],
+      (error, results) => {
+        if (error) {
+          console.log(error);
+          res.status(500).json({ status: "internal server error" });
+        } else {
+          res.status(201).json({
+            status: "success",
+            recipeCreated: req.body.recipeCreated,
+          });
+        }
+      }
+    );
   }
-});
+);
 
 module.exports = recipeRouter;
